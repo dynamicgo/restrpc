94426d0f50e89ea9f6af15c7e7e0e276abf1ab41
yayanyang <yayanyang@gmail.com>
2018-10-25 20:27:46 +0800

    update

--- validator/validator.go
+++ validator/validator.go
@@ -3,45 +3,104 @@ package validator
 import (
 	"errors"
 	"reflect"
+	"strings"
 
 	"github.com/dynamicgo/xerrors"
 )
 
 // Errors
 var (
-	ErrValidator = errors.New("invalid valididator,this inner error")
-	ErrNotFound  = errors.New("resource not found")
+	ErrInvalidType = errors.New("invalid parameter type")
 )
 
-// the validator include three part: reflect type validator, data reader and controller
+// MetadataTag .
+const MetadataTag = "rest"
 
-// Validator type validator
+// Metadata .
+type Metadata struct {
+	Required bool   // required parameter flag
+	Name     string // parameter name
+}
+
+// ParseMetadata .
+func ParseMetadata(tag string) *Metadata {
+	metadata := &Metadata{}
+
+	if tag == "" {
+		return metadata
+	}
+
+	tokens := strings.Split(tag, ",")
+
+	for _, token := range tokens {
+		switch token {
+		case "required":
+			metadata.Required = true
+		default:
+			metadata.Name = token
+		}
+	}
+
+	return metadata
+}
+
+// Validator .
 type Validator interface {
-	Validate(reader Reader, parmT reflect.Type) (reflect.Value, error)
+	Validate(reader Reader, paramT reflect.Type) ([]reflect.Value, error)
 }
 
-// Validatable the type implement validatable
-type Validatable interface {
-	Validate() error
-}
-
-// Reader input data reader
+// Reader parameter reader
 type Reader interface {
-	Get(path string) ([]string, error)
-	GetReader(path string) Reader
+	Search(key string) ([]string, error)
+	Get() ([]string, error)
+	Path() string // reader path
 }
 
-// NewValidator create new validator for given parameter type
-func NewValidator(parmT reflect.Type) (Validator, error) {
+// Validate validate parameter with reflect type and reader
+func Validate(reader Reader, paramT reflect.Type) ([]reflect.Value, error) {
 
-	if parmT.Kind() == reflect.Ptr {
-		parmT = parmT.Elem()
+	if paramT.Kind() == reflect.Ptr {
+		paramT = paramT.Elem()
 	}
 
-	switch parmT.Kind() {
+	var validator Validator
+
+	switch paramT.Kind() {
+	case reflect.Int, reflect.Int8,
+		reflect.Int16, reflect.Int32,
+		reflect.Int64, reflect.Uint,
+		reflect.Uint8, reflect.Uint16,
+		reflect.Uint32, reflect.Uint64,
+		reflect.Float32, reflect.Float64:
 	case reflect.Struct:
-		return &structValidator{}, nil
+	case reflect.String:
+	case reflect.Array:
+	case reflect.Map:
 	default:
-		return nil, xerrors.Wrapf(ErrNotFound, "unsupport type %s validator", parmT)
+		if paramT.Implements(reflect.TypeOf(&validator).Elem()) {
+			value := reflect.New(paramT)
+			validator = value.Interface().(Validator)
+		}
 	}
+
+	if validator == nil {
+		return nil, xerrors.Wrapf(ErrInvalidType, "not support parameter type %s with path %s", paramT, reader.Path())
+	}
+
+	return validator.Validate(reader, paramT)
+}
+
+type numberValidator struct {
+}
+
+type structValidator struct {
+}
+
+type stringValidator struct {
+}
+
+type arrayValidator struct {
+}
+
+type mapValidator struct {
 }
